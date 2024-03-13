01d0ac1fcdb3cea29cb6daa3c72b12768df49685
GavrilS <gavr1lll@abv.bg>
2024-03-13 23:59:11 +0200

    Added action to the manifest file - popup.html

--- script.js
+++
@@ -1,8 +0,0 @@
-// Get all tabs' urls
-chrome.tabs.query({}, (tabs) => {
-    tabs.forEach((tab) => {
-        console.log(tab);
-        console.log(tab.url);
-        console.log('******************************');
-    });
-});
