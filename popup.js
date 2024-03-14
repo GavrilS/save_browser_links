538cb4a624e32a2d49d01ef3d6902a9d600edf12
GavrilS <gavr1lll@abv.bg>
2024-03-14 19:56:38 +0200

    Adding functionality -> WIP

--- popup.js
+++ popup.js
@@ -1,18 +1,18 @@
 // Get all tabs' urls
-// chrome.tabs.query({}, (tabs) => {
-//     tabs.forEach((tab) => {
-//         console.log(tab);
-//         console.log(tab.url);
-//         console.log('******************************');
-//     });
-// });
-
-const tabs = chrome.tabs.query({});
+const tabList = [];
+chrome.tabs.query({}, (tabs) => {
+    tabs.forEach((tab) => {
+        // console.log(tab);
+        // console.log(tab.url);
+        // console.log('******************************');
+        tabList.push(tab);
+    });
+});
 
 const template = document.getElementById('template-list');
 const elements = new Set();
 
-for (const tab of tabs) {
+for (const tab of tabList) {
     const element = template.content.firstElementChild.cloneNode(true);
 
     const title = tab.title.trim();
@@ -20,4 +20,17 @@ for (const tab of tabs) {
 
     element.querySelector('.title').content = title;
     element.querySelector('.url-path').content = url;
+    element.querySelector('a').addEventListener('click', async () => {
+        await chrome.tabs.update(tab.id, {active: true});
+        await chrome.windows.update(tab.windowId, {focused: true});
+    });
+
+    elements.add(element);
 }
+// document.querySelector('ul').append(...elements);
+console.log(document.querySelector('ul'));
+
+const saveUrlsBtn = document.getElementById('save-urls');
+const reloadUrlsBtn = document.getElementById('reload-urls');
+
+
